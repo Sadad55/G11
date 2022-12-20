a34fe6d2318ff3f7b689696b73391637df672570
Sadad55 <121070071+Sadad55@users.noreply.github.com>
2022-12-20 09:17:12 +0700

    Add files via upload

---
+++ G1.py
@@ -0,0 +1,12 @@
+mport random
+import sys
+import time
+def mengetik(S):
+   for c in s + '\n':
+     sys.stdout.write(c)
+     sys.stdout.flush()
+
+
+     time.sle(random.random() * 0,3)
+
+mengetik('Jangan lupa subcribe Chenel User eror21\nselamat menonton\nTerimakasih.')
